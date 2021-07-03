7b6fbeff42f84e5b65aa7945fe8cfa77aaac8270
Irene Riportella <riportellairene@gmail.com>
2021-07-03 17:55:30 +0200

    fixed homepage using string interpolation

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -10,7 +10,7 @@ function getProducts() {
   //Je déclare l'URL de l'API, j'utilise fetch
 
   return (
-    fetch("http://localhost:3000/api/teddies")
+    fetch("http://localhost:3000/api/teddies/")
       //Les promesses
 
       .then(function (httpBodyResponse) {
@@ -30,82 +30,112 @@ function getProducts() {
 }
 
 function displayProducts(products) {
+  // console.log(products);
   products.forEach(function (produit) {
-    //Création des éléments HTML
-
-    let container = document.getElementById("container");
-
-    let carte = document.createElement("div");
-
-    let bottomCarte = document.createElement("div");
-
-    let texteDiv = document.createElement("div");
-
-    let image = document.createElement("img");
-
-    let titre = document.createElement("h3");
-
-    let description = document.createElement("p");
-
-    let prix = document.createElement("h4");
-
-    let btn = document.createElement("a");
-
-    //Structure des éléments dans le HTML
-
-    container.appendChild(carte);
-
-    carte.appendChild(image);
-
-    carte.appendChild(texteDiv);
-
-    texteDiv.appendChild(titre);
-
-    texteDiv.appendChild(description);
-
-    texteDiv.appendChild(bottomCarte);
-
-    bottomCarte.appendChild(prix);
-
-    bottomCarte.appendChild(btn);
-
-    //Déclaration d'attributs
-
-    carte.setAttribute("class", "carte");
-
-    image.setAttribute("class", "image");
-
-    texteDiv.setAttribute("class", "content");
-
-    titre.setAttribute("class", "titre");
-
-    description.setAttribute("class", "description");
-
-    bottomCarte.setAttribute("class", "bottom-carte");
-
-    prix.setAttribute("class", "prix");
-
-    btn.setAttribute("button", "click");
-
-    btn.setAttribute("class", "btn");
-
-    /*Je récupère les informations des produits depuis l'API pour les afficher ensuite*/
-
-    titre.textContent = produit.name;
-
-    description.textContent = produit.description;
-
-    prix.textContent = "Prix: " + produit.price / 100 + " €";
-
-    image.src = produit.imageUrl;
-
-    btn.textContent = "Voir produit";
+    // console.log(produit);
+    const container = document.querySelector("#container");
+    const price = produit.price / 100 + " €";
+    container.innerHTML += `<div class="carte">
+    <img class="image" src="${produit.imageUrl}" alt="image d'un ourson">
+    <div class="content">
+      <h3 class="titre">${produit.name}</h3>
+      <p class="description">${produit.description}</p>
+      <div class="bottom-carte">
+        <h4 class="prix">${price}</h4>
+        <a button="click" class="btn">Voir produit</a>
+      </div>
+    </div>
+  </div>`;
+    // console.log(container);
 
     //Ècoute de l'événement (click) pour que l'utilisateur sélectionne son produit et redirection vers la page produit
 
+    let btn = document.querySelector(".btn");
+
     btn.addEventListener("click", function () {
-      btn.href = "./produit.html?id" + produit._id;
+      btn.href = "./produit.html?id=" + produit._id;
     });
     console.log(produit._id);
   });
 }
+
+// function displayProducts(products) {
+//   products.forEach(function (produit) {
+//     //Création des éléments HTML
+
+//     let container = document.getElementById("container");
+
+//     let carte = document.createElement("div");
+
+//     let bottomCarte = document.createElement("div");
+
+//     let texteDiv = document.createElement("div");
+
+//     let image = document.createElement("img");
+
+//     let titre = document.createElement("h3");
+
+//     let description = document.createElement("p");
+
+//     let prix = document.createElement("h4");
+
+//     let btn = document.createElement("a");
+
+//     //Structure des éléments dans le HTML
+
+//     container.appendChild(carte);
+
+//     carte.appendChild(image);
+
+//     carte.appendChild(texteDiv);
+
+//     texteDiv.appendChild(titre);
+
+//     texteDiv.appendChild(description);
+
+//     texteDiv.appendChild(bottomCarte);
+
+//     bottomCarte.appendChild(prix);
+
+//     bottomCarte.appendChild(btn);
+
+//     //Déclaration d'attributs
+
+//     carte.setAttribute("class", "carte");
+
+//     image.setAttribute("class", "image");
+
+//     texteDiv.setAttribute("class", "content");
+
+//     titre.setAttribute("class", "titre");
+
+//     description.setAttribute("class", "description");
+
+//     bottomCarte.setAttribute("class", "bottom-carte");
+
+//     prix.setAttribute("class", "prix");
+
+//     btn.setAttribute("button", "click");
+
+//     btn.setAttribute("class", "btn");
+
+//     /*Je récupère les informations des produits depuis l'API pour les afficher ensuite*/
+
+//     titre.textContent = produit.name;
+
+//     description.textContent = produit.description;
+
+//     prix.textContent = "Prix: " + produit.price / 100 + " €";
+
+//     image.src = produit.imageUrl;
+
+//     btn.textContent = "Voir produit";
+
+//     //Ècoute de l'événement (click) pour que l'utilisateur sélectionne son produit et redirection vers la page produit
+
+//     btn.addEventListener("click", function () {
+//       btn.href = "./produit.html?id=" + produit._id;
+//     });
+//     console.log(produit._id);
+//   });
+// }
--- front-end/js/produit.js
+++ front-end/js/produit.js
@@ -1,16 +1,21 @@
 //récupération de l'ID de l'ourson de la page
 
-const url = window.location.search;
-const id = new URLSearchParams(url);
-const produit = id.get("id");
+// const url = window.location.search;
+// const id = new URLSearchParams(url);
+// const produit = id.get("id");
+// console.log(produit);
+
+let params = new URL(window.location).searchParams;
+let produit = params.get("id");
 console.log(produit);
 
-fetch(`http://localhost:3000/${produit._id}`)
+fetch(`http://localhost:3000/api/teddies/${produit}`)
   .then((httpBodyResponse) => {
     return httpBodyResponse.json();
   })
   .then((produit) => {
-    displayProduct([produit]);
+    console.log(produit);
+    displayProduct(produit);
   })
   .catch((error) => {
     console.log(error);
@@ -18,31 +23,22 @@ fetch(`http://localhost:3000/${produit._id}`)
 
 //création de la structure HTML du produit
 
-// function displayProduct(produits) {
-//   produits.forEach((produit) => {
-//     const container = document.querySelector("#container");
-//     container.innerHTML = `<div>
-//           <img class="image" src="" alt="image d'un ourson" />
-//           <h3 class="titre"></h3>
-//           <p class="description"></p>
-//           <select name="couleur" id="couleur"></select>
-//           <select name="quantité" id="quantite"></select>
-//           <h4 class="price"></h4>
-//           <button class="addtocart" type="submit" ></button>
-//         </div>`;
-//     console.log(container);
-
-//     titre.textContent = produit.name;
-
-//     description.textContent = produit.description;
-
-//     prix.textContent = produit.price / 100 + " €";
-
-//     image.src = produit.imageUrl;
-
-//     addToCart.textContent = "Ajouter au panier";
-//   });
-// }
+function displayProduct(produit) {
+  const container = document.querySelector("#containerproduit");
+  const price = produit.price / 100 + " €";
+  containerproduit.innerHTML = `<div class="containerproduit">
+          <img class="image" src="${produit.imageUrl}" alt="image d'un ourson" />
+          <div class="info">
+            <h3 class="titre">${produit.name}</h3>
+            <p class="description">${produit.description}</p>
+            <select name="couleur" id="couleur"></select>
+            <select name="quantité" id="quantite"></select>
+            <h4 class="price">${price}</h4>
+            <button class="addtocart" type="submit" >Ajouter au panier</button>
+          </div>
+        </div>`;
+  console.log(container);
+}
 
 //création choix produits
 // function selectColor(produits) {
